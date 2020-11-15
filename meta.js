09f6918baccb8b404f843c394772d8ffcdcd3ccb
Steffen Schön <steffen1995@gmail.com>
2020-11-15 13:40:55 +0100

    improve meta.js

--- meta.js
+++ meta.js
@@ -1,3 +1,42 @@
+const spawn = require("child_process").spawn;
+
+/**
+ * Spawns a child process and runs the specified command
+ * By default, runs in the CWD and inherits stdio
+ * Options are the same as node's child_process.spawn
+ * @param {string} cmd
+ * @param {array<string>} args
+ * @param {object} options
+ */
+function runCommand(cmd, args, options) {
+  return new Promise((resolve, reject) => {
+    const spwan = spawn(
+      cmd,
+      args,
+      Object.assign(
+        {
+          cwd: process.cwd(),
+          stdio: 'inherit',
+          shell: true,
+        },
+        options
+      )
+    )
+
+    spwan.on('exit', () => {
+      resolve()
+    })
+  })
+}
+
+const installDependencies = () => {
+  console.log(`\n\n# ${color("Installing project dependencies ...")}`);
+  console.log("# ========================\n");
+  return runCommand("npm", ["install"], {
+    cwd,
+  });
+};
+
 /**
  * Meta JS File that will be picked up by the Vue CLI
  */
@@ -8,15 +47,27 @@ module.exports = {
       required: true,
       message: 'Project name'
     },
-    templatename: {
+    templatedescription: {
       type: 'string',
       required: false,
       message: 'Description',
     },
-    templatename: {
+    templateauthor: {
       type: 'string',
       message: 'Author',
       required: true
     },
+  },
+  complete: function(data, { chalk }){
+    const green = chalk.green;
+    const cwd = path.join(process.cwd(), data.inPlace ? "" : data.destDirName);
+
+    installDependencies(cwd, data.autoInstall, green)
+      .then(() => {
+        console.log("green('Project initialization finished!')}");
+      })
+      .catch((e) => {
+        console.log(chalk.red("Error:"), e);
+      });
   }
 };
